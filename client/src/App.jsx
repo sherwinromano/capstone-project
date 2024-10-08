71471acf594bc38dcbfff3feddf883e6189a6176
Jokbal88 <157182639+Jokbal88@users.noreply.github.com>
2024-10-08 20:23:53 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AuthLayout from "./pages/Authentication/AuthLayout";
 import SignIn from "./pages/Authentication/SignIn";
 import SignUp from "./pages/Authentication/SignUp";
@@ -8,6 +8,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Redirect from root ("/") to "/auth/sign-in" */}
+        <Route path="/" element={<Navigate to="/auth/sign-in" />} />
+
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="sign-in" element={<SignIn />} />
           <Route path="sign-up" element={<SignUp />} />
