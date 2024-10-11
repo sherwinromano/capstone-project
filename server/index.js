4fef90b6defb66cabf512f9633b365709e0fc269
Jokbal88 <157182639+Jokbal88@users.noreply.github.com>
2024-10-11 10:56:33 +0800

    Removed MySQL Connection
    Removed MySQL connection, changing database for the system

--- server/index.js
+++ server/index.js
@@ -8,23 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Create MySQL connection
-const db = mysql.createConnection({
-  host: 'localhost', // Replace with your MySQL host
-  user: 'root',      // Replace with your MySQL username
-  password: 'test123',      // Replace with your MySQL password
-  database: 'studentaccounts' // Replace with your MySQL database name
-});
-
-// Connect to MySQL
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
 // Test route
 app.get('/', (req, res) => {
   res.send('Hello from the server');
